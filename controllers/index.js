6665343410e9a91f998bca7d00b202f2cdafe76b
Sonu Tiwari <ankorha@gmail.com>
2020-03-27 02:04:24 +0530

    Working code for captcha

--- controllers/index.js
+++ controllers/index.js
@@ -1,35 +1,85 @@
 const userModel = require('../models/user_model');
+const db = require('../configs/db_config');
 const AuthController = {};
+const THRESHOLD = 3;
 AuthController.showRegistrationPage = (req, res) => {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    let showCaptcha = false;
-    if (ip) {
-        if (isRequestThreshHoldCrossed(ip)){
-            showCaptcha = true;
+    var now = new Date();
+    var startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    userModel.countDocuments({ip: ip, createdAt: {$gte: startOfToday}}, function (err, count) { 
+        console.log(count, "Count", "IP", ip);
+        if (count >= THRESHOLD) {
+            return res.render('sign_up', { 
+                title: 'Sign Up', 
+                showCaptcha: true, 
+                recaptcha: res.recaptcha
+            });
+        } else {
+            return res.render('sign_up', { 
+                title: 'Sign Up', 
+                showCaptcha: false, 
+                recaptcha: res.recaptcha
+            });
         }
-    }
-    return res.render('sign_up', {title: 'Sign Up', showCaptcha: showCaptcha});
+    });
 }
 
 AuthController.registerUser = (req, res) => {
-    return res.send("Working");
+    if (!req.recaptcha.error) {
+        // success code
+        if (isValidPassword(req.body.password)){ // Rest of the data is validated on front end.
+            let data = req.body;
+            data.ip = ip;
+            let document = new userModel(data);
+            document.save((err, resp) => {
+                if (err) {
+                    console.log('Error in saving data, ', err);
+                    return res.redirect('/');
+                } else {
+                    return res.redirect('/');
+                }
+            });
+        }
+        console.log('Success');
+      } else {
+        // error code
+        let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        var now = new Date();
+        var startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+        userModel.countDocuments({ip: ip, createdAt: {$gte: startOfToday}}, function (err, count) { 
+            if (count < THRESHOLD) {
+                if (req.body) {
+                    if (isValidPassword(req.body.password)){ // Rest of the data is validated on front end.
+                        let data = req.body;
+                        data.ip = ip;
+                        let document = new userModel(data);
+                        document.save((err, resp) => {
+                            if (err) {
+                                console.log('Error in saving data, ', err);
+                                return res.redirect('/');
+                            } else {
+                                return res.redirect('/');
+                            }
+                        });
+                    }
+                }
+                console.log(req.body);
+            } else {
+                console.log('Error', req.recaptcha.error);
+                return res.redirect('/');
+            }
+        });
+        
+      }
 }
 
-AuthController.registerSuccess = (req, res) => {
-    return res.send("Working");
-}
 
 AuthController.pageNotFound = (req, res) => {
     return res.status(404).send("Page not found");
 }
 
-function isRequestThreshHoldCrossed(ip){
-    return new Promise(resolve=>{
-        var now = new Date();
-        var startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-        userModel.countDocuments({created_on: {$gte: startOfToday}}, function (err, count) { 
-            resolve(count > 3);
-        });
-    })
+function isValidPassword(password) {
+    var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    return password.match(regex);
 }
 module.exports = AuthController;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const authController = require('../controllers/index');
 const Router = require('express').Router();
-Router.get('/', authController.showRegistrationPage);
-Router.post('/register', authController.registerUser);
-Router.get('/register_success', authController.registerSuccess);
+var Recaptcha = require('express-recaptcha').RecaptchaV2;
+//import Recaptcha from 'express-recaptcha'
+const API_KEY = '6LeGTeQUAAAAADkEPhrGDWC6isBisbF783xZVi18';
+const SECRET_KEY = '6LeGTeQUAAAAAB9uroVWWcrBjzQ9jSnBuH_8y-9z';
+var recaptcha = new Recaptcha(API_KEY, SECRET_KEY, {callback: 'cb'});
+Router.get('/', recaptcha.middleware.render, authController.showRegistrationPage);
+Router.post('/register', recaptcha.middleware.verify, authController.registerUser);
 Router.get('/*', authController.pageNotFound);
 module.exports  = Router;
