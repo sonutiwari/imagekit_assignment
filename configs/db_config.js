86a5d45f754e18aadedc740ff864fd4ff68aa4fb
Sonu Tiwari <ankorha@gmail.com>
2020-03-29 11:37:20 +0530

    Heroku is asking for Credit card so switching back to local

--- configs/db_config.js
+++ configs/db_config.js
@@ -1,14 +1,11 @@
 // Get mongoose lib.
 const mongoose = require('mongoose');
 
-// Conection String.
-const CONNECTION_STRING = "mongodb+srv://sonutj:Sonu%401996@restaurant-lcbzk.mongodb.net/test?retryWrites=true&w=majority";
-
 // local url.
 const LOCAL_URL = 'mongodb://127.0.0.1:27017/imagekit';
 
 // Connect to the localhost.
-mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true});
+mongoose.connect(LOCAL_URL, {useNewUrlParser: true});
 
 // Get connection object.
 const db = mongoose.connection;
